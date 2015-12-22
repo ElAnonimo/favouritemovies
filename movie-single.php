8d3c8ed5501c325baa7bb022d9057effca00ab4a
ElAnonimo <bnnm1234@mail.ru>
2015-12-22 15:39:56 +0300

    Added the .sql to initialize the DB.

--- movie-single.php
+++ movie-single.php
@@ -4,7 +4,5 @@ require_once 'php-includes/header.inc.php';
 require_once 'php-includes/navigation.inc.php';
 require_once 'php-includes/favourites.inc.php';
 ?>
-
-
-        
+  
 <?php require_once 'php-includes/footer.inc.php'; ?>
